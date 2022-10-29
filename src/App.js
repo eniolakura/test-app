10b61e97f7fd4b760a241870db49a5676f0fb237
eniolakura <enybeats76@gmail.com>
2022-10-30 00:20:29 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -7,14 +7,30 @@ function App() {
       <header>
         <img id='profile__img'>
         </img>
+        
       </header>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className='buttons'>
+        <div>
+          <button>Twitter link</button>
+          </div>
+        <div>
+          <button id='btn__zuri'>Zuri Team</button>
+          </div>
+        <div>
+          <button id='books'>Zuri Books</button>
+          </div>
+        <div>
+          <button id='book__python'>Python Books</button>
+          </div>
+        <div>
+          <button id='pitch'>Background Check for coders</button>
+          </div>
+        <div>
+          <button id='book_design'>Design Books</button>
+        </div>
+        <footer>
+
+        </footer>
       </div>
     </div>
   );
